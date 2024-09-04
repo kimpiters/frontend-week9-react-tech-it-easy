e8b544bdba8ef2770dfbd34fa162b0088f4546e2
kimpiters <piters.kim@gmail.com>
2024-09-04 13:27:17 +0200

    everything updated

--- src/helpers/generateTVArticle/GenerateListTvOptions.jsx
+++ src/helpers/generateTVArticle/GenerateListTvOptions.jsx
@@ -4,19 +4,19 @@ import notIcon from "../../assets/minus.png";
 function GenerateListTvOptions(tv) {
     return tv.options.map((option) => {
         if (option.applicable === true) {
-            return (<li key={option}>
+            return <li key={option}>
                 <div className="icon-wrapper">
                 <img src={checkIcon} alt="Check icoon"/>
             </div>
             <div>{option.name}</div>
-        </li>)
+        </li>
         } else {
-            return (<li key={option}>
+            return <li key={option}>
                 <div className="icon-wrapper">
                 <img src={notIcon} alt="Minus icoon"/>
             </div>
             <div>{option.name}</div>
-        </li>)
+        </li>
         }
     })
 }
