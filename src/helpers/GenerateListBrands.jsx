1287c4331721309ff14e3a23a31b4e0d0efe1505
kimpiters <piters.kim@gmail.com>
2024-09-04 12:17:56 +0200

    Everything updated

--- src/helpers/GenerateListBrands.jsx
+++ src/helpers/GenerateListBrands.jsx
@@ -1,18 +1,10 @@
 import {inventory} from "../constants/inventory.js";
 
 function GenerateListBrands() {
-    let listBrands = [];
-    let counter = 0;
-    for (const tv of inventory) {
-        if (listBrands.length === 0) {
-            listBrands.push(<li key={counter}>{tv.brand}</li>);
-            counter++;
-        } else {
-            listBrands.push(<li key={counter}>{tv.brand}</li>);
-            counter++;
-        }
-    }
-    return listBrands;
+
+    return inventory.map((tv) => {
+        return <li key={tv.type}>{tv.brand}</li>;
+    })
 }
 
 export default GenerateListBrands;
---
+++ src/helpers/generateTVArticle/GenerateListTvOptions.jsx
@@ -0,0 +1,24 @@
+import checkIcon from "../../assets/check.png";
+import notIcon from "../../assets/minus.png";
+
+function GenerateListTvOptions(tv) {
+    return tv.options.map((option) => {
+        if (option.applicable === true) {
+            return (<li key={option}>
+                <div className="icon-wrapper">
+                <img src={checkIcon} alt="Check icoon"/>
+            </div>
+            <div>{option.name}</div>
+        </li>)
+        } else {
+            return (<li key={option}>
+                <div className="icon-wrapper">
+                <img src={notIcon} alt="Minus icoon"/>
+            </div>
+            <div>{option.name}</div>
+        </li>)
+        }
+    })
+}
+
+export default GenerateListTvOptions;
